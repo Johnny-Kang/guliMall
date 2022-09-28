2f099491e2183079f96a7004e24f75a36415dd75
Johnny-Kang <2578998609@qq.com>
2022-09-28 18:09:14 +0800

    项目初始结构创建

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,13 +1,38 @@
 <template>
-    <dev>
-      商品服务
-    </dev>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
 </template>
 
 <script>
-    export default {
-        name: ""
+  export default {
+    data() {
+      return {
+        data: [],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      };
+    },
+    created(){
+      this.getDataList()
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      },
+      // 获取数据列表
+      getDataList () {
+        this.dataListLoading = true
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get',
+        }).then(({data}) => {
+          console.log(data);
+          this.dataListLoading = false
+        })
+      },
     }
+  };
 </script>
 
 <style scoped>
